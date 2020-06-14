0a94d72d0338934aeadcc5b8b768dedf6eac6075
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-06-14 17:19:03 +0100

    All tests

--- test.js
+++ test.js
@@ -1,31 +1,9 @@
 const gen = require('./gen');
-// const [name, id] = process.argv.slice(2);
+const cities = require('./cities.json');
 
-const tests = [
-  {
-    id: 5,
-    name: 'Berkane',
-  },
-  {
-    id: 104,
-    name: 'Boujdour',
-  },
-  {
-    id: 23,
-    name: 'Errachidia',
-  },
-  {
-    id: 11,
-    name: 'Guersif',
-  },
-  {
-    id: 35,
-    name: 'Ifrane',
-  },
-  {
-    id: 45,
-    name: 'Khenifra',
-  },
-];
-
-tests.forEach(async (city) => await gen(city.name, city.id));
+cities
+  .map((c) => ({
+    id: c.id,
+    name: c.slug,
+  }))
+  .forEach(async (city) => await gen(city.name, city.id));
